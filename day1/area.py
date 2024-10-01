2ef76bcf0a1d82b077ef90ac7a2af75109972fac
vachan-maker <vachan@tutanota.com>
2024-10-01 20:13:26 +0530

    initial commit

---
+++ day1/area.py
@@ -0,0 +1,4 @@
+x = float(input("Enter length: "))
+y = float(input("Enter breadth: "))
+area = x *y
+print(f"Area is {area}")
---
+++ day1/basics.py
@@ -0,0 +1,10 @@
+length = 10
+height = 5
+width = 2
+
+print(length * width * height)
+print('hello' * 2)
+print(type(width))
+x = input("Enter age: ")
+print(int(x)+10)
+# blocking function
