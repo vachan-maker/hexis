fe65974894919ccdf4bcd5d18654ead965886c94
vachan-maker <vachan@tutanota.com>
2024-10-03 21:09:06 +0530

    day3

---
+++ day3/list.py
@@ -0,0 +1,12 @@
+names = ["Alice", "Bob", "Charlie","David","Eve"]
+print(names[0])
+print(names)
+
+# iterate over each element
+for name in names:
+    print(name)
+
+print(names[2:4])
+
+print(names[:2])
+# prints the last two elements
