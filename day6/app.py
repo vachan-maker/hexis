f8bacb510a9712b5a0ed824cd911bf1cef734184
vachan-maker <vachan@tutanota.com>
2024-10-07 21:42:39 +0530

    form submission

--- day6/app.py
+++ day6/app.py
@@ -8,5 +8,13 @@ def index():
     namesList = ["Solomon","Hari", "Bob"]
     return render_template('index.html', names = namesList)
 
+@app.route("/insert", methods=['POST'])
+def insert():
+    return render_template("insert.html")
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
