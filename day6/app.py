83be512f482e650cbd1b5d7241aa0191014a64d0
vachan-maker <vachan@tutanota.com>
2024-10-07 22:01:16 +0530

    tried to write to a json file but it doesn't seem to work

--- day6/app.py
+++ day6/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,request,redirect,url_for
 import pip._vendor.requests as requests
+import json
 
 app = Flask(__name__)
 
@@ -12,6 +13,13 @@ def index():
 def insert():
     name = request.form["name"]
     place = request.form["place"]
+    details = {
+        "name":name,
+        "place":place
+    }
+    with open('data.json','w') as f:
+        json_data = json.dumps(details)
+        f.write(json_data)
     return render_template("insert.html", userName = name, userPlace = place)
 
 @app.route("/form")
